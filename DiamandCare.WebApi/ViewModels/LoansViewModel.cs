f2a3af61fbd8fd1533f71a43da39c3847e495f5b
sivakishore <sivakishore.teru@hotmail.com>
2018-08-11 15:01:23 +0530

    loans view model

--- DiamandCare.WebApi/ViewModels/LoansViewModel.cs
+++ DiamandCare.WebApi/ViewModels/LoansViewModel.cs
@@ -9,23 +9,34 @@ namespace DiamandCare.WebApi
     {
         public int LoanID { get; set; }
         public int UserID { get; set; }
-        public int GroupsID { get; set; }
-        public decimal LoanAmount { get; set; }
-        public decimal IssuedAmount { get; set; }
-        public decimal AmountToPay { get; set; }
-        public decimal AdminCharges { get; set; }
+        public string UserName { get; set; }
+        public int GroupID { get; set; }
+        public double LoanAmount { get; set; }
+        public double IssuedAmount { get; set; }
+        public double AmountToPay { get; set; }
+        public double AdminCharges { get; set; }
         public int ModeofTransfer { get; set; }
+        public string ModeofTransferType { get; set; }
         public int LoanStatusID { get; set; }
+        public string LoanStatusName { get; set; }
         public string LoanTypeCode { get; set; }
+        public string LoanTypeDescription { get; set; }
+        public bool PrePaidLoan { get; set; }
         public int CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
         public int UpdatedBy { get; set; }
         public DateTime UpdatedOn { get; set; }
-        public bool PrePaidLoan { get; set; }
         public int IsApproved { get; set; }
+        public string LoanApprovalStatus { get; set; }
         public decimal SGST { get; set; }
         public decimal CGST { get; set; }
         public decimal IGST { get; set; }
         public decimal TDS { get; set; }
+        public int ApproveOrRejectedBy { get; set; }
+        public DateTime ApproveOrRejectedOn { get; set; }
+        public int TransferBy { get; set; }
+        public DateTime TransferOn { get; set; }
+        public int TransferStatusID { get; set; }
+        public string TransferStatus { get; set; }
     }
 }

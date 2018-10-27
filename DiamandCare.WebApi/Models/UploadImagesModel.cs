690a5e991296567541c5b1a8643b9d3801c4fe01
Purushottam Koruprolu <klpnaidu84@gmail.com>
2018-10-27 13:06:46 +0530

    Latest changes in  api

---
+++ DiamandCare.WebApi/Models/UploadImagesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiamandCare.WebApi
+{
+    public class UploadImagesModel
+    {
+        public int InstituteID { get; set; }
+        public string InstituteName { get; set; }
+        public string FileName { get; set; }
+        public string Url { get; set; }
+    }
+}
